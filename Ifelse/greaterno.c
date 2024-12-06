64d50d131a7961c1158054c34c6cc5891f37bf19
Aman <amanhongal@gmail.com>
2024-12-06 11:33:16 +0530

    Basic condition problem to find Greater is solved using if else statement

---
+++ Ifelse/greaterno.c
@@ -0,0 +1,16 @@
+// find the greater number from the 2
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value for a and b : \n");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+    {
+        printf("a is Greater than b");
+    }
+    else{
+        printf("b is Greater than a ");
+    }
+    return 0;
+    
+}
