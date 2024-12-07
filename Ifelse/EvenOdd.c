158d04d0004aa2faacfb3d946eae5be45dcca8fa
Aman <amanhongal@gmail.com>
2024-12-07 09:02:20 +0530

    Even Odd verification problem is done

---
+++ Ifelse/EvenOdd.c
@@ -0,0 +1,13 @@
+// find the given number is evan or odd 
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number : \n");
+    scanf("%d",&a);
+    if(a%2==0)
+        printf("Number is Even");
+    else
+        printf("Number is Odd");;
+    return 0;
+}
