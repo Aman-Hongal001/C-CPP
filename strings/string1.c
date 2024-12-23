35483b926974a7431d35480d6231788f0826a8b5
Aman <amanhongal@gmail.com>
2024-12-23 10:04:57 +0530

    learn about puts and gets

--- strings/string1.c
+++ strings/string1.c
@@ -3,7 +3,9 @@ int main()
 {
     char name[20];
     printf("Enter name : ");
-    scanf("%s",name);
+    // scanf("%s",name);
+    gets(name);
     printf("Your name is %s\n",name);
+    puts(name);
     return 0;
 }
