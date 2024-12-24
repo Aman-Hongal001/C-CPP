171e4269f2f6f8d20b601696079657932ff63a3c
Aman <amanhongal@gmail.com>
2024-12-24 10:39:00 +0530

    Function to create the string into lowercase is done

--- strings/strLwr.c
+++ strings/strLwr.c
@@ -1,4 +1,4 @@
-// Write a function to print the str to Uppercase 
+// Write a function to print the str to LowerCase 
 #include<stdio.h>
 char* stringUpperCase(char*);
 int main()
@@ -17,8 +17,8 @@ char* stringUpperCase(char* ptr)
     int i=0;
     while (ptr[i]!='\0')
     {
-        if(ptr[i] >= 'a' && ptr[i] <= 'z')
-            ptr[i] -= 32;
+        if(ptr[i] >= 'A' && ptr[i] <= 'Z')
+            ptr[i] += 32;
         i++;
 
     }
